48d0e063858e4c9c3a3495fdfffe8ef0391d043f
sabanferik <sbnftmaf@gmail.com>
2024-02-29 19:06:05 +0100

    PlayerCard tmm

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
 
-const PlayerCard = () => {
+// const PlayerCard = ({player}) => {
+const PlayerCard = ({ name, img, statistics }) => {
+
+const [showImage,setShow]=useState(true)
+
   return (
-    <div>PlayerCard</div>
-  )
-}
+    <Card className="rounded-2 m-auto player-kart" role="button" onClick={() => setShow(!showImage)}>
+      {showImage ? (
+        <Card.Img variant="top" src={img}  className="player-logo"/>
+      ) : (
+        <ul className="m-auto">
+          {statistics.map((item, index) => {
+            return (
+              <li className="h5 text-start list-unstyled" key={index}>
+               
+                🏀{item}
+              </li>
+            );
+          })}
+        </ul>
+      )}
 
-export default PlayerCard
+      <Card.Footer>
+        <Card.Title>{name}</Card.Title>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default PlayerCard;
